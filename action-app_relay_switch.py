3162fb20bef22d406ad43453657422d98dd350ad
CoorFun <coorfang@gmail.com>
2018-12-06 13:29:17 +0100

    [Update] house_room

--- action-app_relay_switch.py
+++ action-app_relay_switch.py
@@ -16,10 +16,10 @@ MQTT_IP_ADDR = "localhost"
 MQTT_PORT = 1883
 MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
-SNIPS_USER = ''
+SNIPS_USER_PREFIX = '{}:'.format('')
 
-GOOD_THRESHOLD = 0.4
-BAD_THRESHOLD = 0.2
+GOOD_THRESHOLD = 0.3
+BAD_THRESHOLD = 0.1
 
 COEFFICIENT = 10
 
@@ -32,20 +32,24 @@ class RelaySwitch(object):
             print 'No "config.ini" found!'
             sys.exit(1)
 
-        self.relay_pin = int(self.config.get('secret',{"gpio_bcm_relay":"12"}).get('gpio_bcm_relay','12'))
+        self.relay_pin = int(self.config.get('global',{"gpio_bcm_relay":"12"}).get('gpio_bcm_relay','12'))
 
-        if self.config.get('secret',{"activate_level":"high"}).get('activate_level','high') == 'high':
+        if self.config.get('global',{"activate_level":"high"}).get('activate_level','high') == 'high':
             self.relay_on = GPIO.HIGH
             self.relay_off = GPIO.LOW
-        elif self.config.get('secret',{"activate_level":"high"}).get('activate_level','high') == 'low':
+        elif self.config.get('global',{"activate_level":"high"}).get('activate_level','high') == 'low':
             self.relay_on = GPIO.HIGH
             self.relay_off = GPIO.LOW
 
-        self.site_id = self.config.get('secret',{"site_id":"living room"}).get('site_id','living room')
+        self.snips_user = str(self.config.get('global',{"snips_user_name":""}).get('snips_user_name',''))
+        if self.snips_user is not '':
+            self.snips_user += ':'
+
+        self.site_id = str(self.config.get('secret',{"site_id":"default"}).get('site_id','default'))
+        self.site_id = str(self.config.get('secret',{"device_name":"default"}).get('device_name','default'))
         self.gpioInit()
-        self.move_to_percentage(100)
         self.start_blocking()
-        
+
     # -> hardware related
     def gpioInit(self):
         GPIO.setmode(GPIO.BCM)
@@ -54,8 +58,14 @@ class RelaySwitch(object):
 
     # -> extraction of slots value
     def extractHouseRoom(self, intent_message, default_value):
-        pass
-        return house_room
+        #if intent_message.slots.house_room:
+        #    return intent_message.slots.house_room.first()
+        return default_value
+
+    def extractDevice(self, intent_message, default_value):
+        #if intent_message.slots.device:
+        #    return intent_message.slots.device.first()
+        return default_value
 
     # -> action callbacks
     def turnOnRelay(self, hermes, intent_message):
@@ -70,16 +80,19 @@ class RelaySwitch(object):
 
     def master_intent_callback(self, hermes, intent_message):
         if intent_message.intent.probability < BAD_THRESHOLD:
+            print "[Return] Possibility is too bad!"
             return
         elif intent_message.intent.probability <= GOOD_THRESHOLD:
-            hermes.publish_end_session(intent_message.session_id, "Sorry, I dont think I understood")
+            print "[Return] Possibility is not good enough!"
+            hermes.publish_end_session(intent_message.session_id, "Sorry, I am not sure I understood")
             return
         if self.extractHouseRoom(intent_message, self.site_id) != intent_message.site_id:
+            print "[Return] Slot [house_room] Unmatch {} != {}".format(self.extractHouseRoom(intent_message, self.site_id), intent_message.site_id)
             return
 
-        if intent_message.intent.intent_name == '{}turnOnRelay'.format(SNIPS_USER):
+        if intent_message.intent.intent_name == '{}relayTurnOn'.format(SNIPS_USER_PREFIX):
             self.turnOnRelay(hermes, intent_message)
-        if intent_message.intent.intent_name == '{}turnOffRelay'.format(SNIPS_USER):
+        if intent_message.intent.intent_name == '{}relayTurnOff'.format(SNIPS_USER_PREFIX):
             self.turnOffRelay(hermes, intent_message)
 
     def start_blocking(self):
